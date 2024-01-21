9b1b610fed20997fff2f07c9968af2dbdc631510
Артур <olmasovarthur@yandex.ru>
2024-01-21 22:21:33 +0500

    попытка отрисовать 3

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import { render, RenderPosition } from './render.js';
+import { render, RenderPosition } from '../src/render.js';
 
 import ComponentsPresenter from './presenter/render-presenter.js';
 
 import SortView from './view/sort-view.js';
-import CreateNewFormView from './view/create-new-form-view.js';
+/*import CreateNewFormView from './view/create-new-form-view.js';
 import EditFormView from './view/edit-form-view.js';
 import FilterView from './view/filter-view.js';
-import PointView from './view/point-view.js';
+import PointView from './view/point-view.js';*/
 
 const sortElement = document.querySelector('.trip-main');
-const createNewFormElement = document.querySelector('.trip-events__item');
+/*const createNewFormElement = document.querySelector('.trip-events__item');
 const editElement = document.querySelector('.trip-events__item');
 const filterElement = document.querySelector('.trip-controls__filters');
-const pointElement = document.querySelector('.trip-events');
+const pointElement = document.querySelector('.trip-events');*/
 
 render(new SortView(), sortElement, RenderPosition.AFTERBEGIN);
 /*render(new CreateNewFormView(), createNewFormElement, RenderPosition.AFTERBEGIN);
--- src/presenter/render-presenter.js
+++ src/presenter/render-presenter.js
@@ -1,4 +1,4 @@
-/*import { render } from '../render.js';
+import { render } from '../render.js';
 import SortView from '../view/sort-view.js';
 import CreateNewFormView from '../view/create-new-form-view.js';
 import EditFormView from '../view/edit-form-view.js';
@@ -25,33 +25,4 @@ export default class ComponentsPresenter {
 
     render(new CreateNewFormView(), this.pointComponent.getElement());
   }
-}*/
-
-import { render } from '../render.js';
-import SortListView from '../view/sort-list-view.js';
-import NewPointFormView from '../view/new-point-form-view.js';
-import PointFormEditView from '../view/point-form-edit-view.js';
-import PointListView from '../view/point-list-view.js';
-import PointItemView from '../view/point-item-view.js';
-
-class EventsPresenter {
-  pointListComponent = new PointListView();
-
-  constructor({eventsContainer}) {
-    this.eventsContainer = eventsContainer;
-  }
-
-
-  init() {
-    render(new SortListView(), this.eventsContainer);
-    render(this.pointListComponent, this.eventsContainer);
-    render(new PointFormEditView(), this.pointListComponent.getElement());
-
-    for (let i = 0; i < 3; i++) {
-      render(new PointItemView(), this.pointListComponent.getElement());
-    }
-
-    render(new NewPointFormView(), this.pointListComponent.getElement());
-  }
 }
-export default EventsPresenter;
